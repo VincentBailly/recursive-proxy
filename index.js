21f8ff8bcd67a664233b904929e8ea919c902365
Vincent Bailly <vincent.bailly@microsoft.com>
2021-12-22 16:54:22 +0100

    refactor

--- index.js
+++ index.js
@@ -3,59 +3,42 @@ exports.recursiveProxy = recursiveProxy
 const memo = new Map()
 
 function recursiveProxy(handler) {
-  const newHandler = {
+  let newHandler = undefined
+
+  function sanitizeInputs(input) {
+    const i = input instanceof Object && input.__proxy_target ? input.__proxy_target : input
+    if (i instanceof Function) {
+      return function (...args) {
+        return i(...args.map(sanitizeOutput))
+      }
+    }
+    return i
+  }
+
+  function sanitizeOutput(output) {
+    if (output instanceof Object) {
+      if (!memo.has(output)) {
+        memo.set(output, new Proxy(output, newHandler))
+      }
+      return memo.get(output)
+    }
+    return output
+  }
+
+  newHandler = {
     ...handler,
     apply: (target, thisArg, argumentList) => {
-      const newArgList = argumentList.map(a => {
-        if (a instanceof Function) {
-          return function (...args) {
-            return a(...args.map(arg => {
-              if (arg instanceof Object) {
-                if (!memo.has(arg)) {
-                  memo.set(arg, new Proxy(arg, newHandler))
-                }
-                return memo.get(arg)
-              }
-              return arg
-            }))
-          }
-        }
-        return a
-      })
+      const newArgList = argumentList.map(sanitizeInputs)
       const value = handler.apply ? handler.apply(target, thisArg, newArgList) : Reflect.apply(target, thisArg, newArgList)
-      if (value instanceof Object) {
-        if (!memo.has(value)) {
-          memo.set(value, new Proxy(value, newHandler))
-        }
-        return memo.get(value)
-      }
-      return value
+      return sanitizeOutput(value)
     },
     get: (target, prop, receiver) => {
       if (prop === '__proxy_target') { return target }
       const value = handler.get ? handler.get(target, prop, receiver) : Reflect.get(target, prop, receiver)
-      if (value instanceof Object) {
-        if (!memo.has(value)) {
-          memo.set(value, new Proxy(value, newHandler))
-        }
-        return memo.get(value)
-      }
-      return value
+      return sanitizeOutput(value)
     },
     set: (target, prop, value) => {
-      const unwrapped = value instanceof Object && value.__proxy_target ? value.__proxy_target : value
-      const sanitized = unwrapped instanceof Function ? 
-        function (...args) {
-          return unwrapped(...args.map(arg => {
-            if (arg instanceof Object) {
-              if (!memo.has(arg)) {
-                memo.set(arg, new Proxy(arg, newHandler))
-              }
-              return memo.get(arg)
-            }
-            return arg
-          }))
-        } : unwrapped
+      const sanitized = sanitizeInputs(value)
       return handler.set ? handler.set(target, prop, sanitized) : Reflect.set(target, prop, sanitized)
     }
   }
